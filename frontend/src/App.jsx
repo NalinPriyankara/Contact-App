66df8da4494a68356b85e9a9cba4246670beb467
NalinPriyankara <112267206+NalinPriyankara@users.noreply.github.com>
2024-07-01 12:05:35 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import ContactList from './ContactList'
 import './App.css'
+import ContactForm from './ContactForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts")
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
 
   return (
     <>
-      
+    <ContactList contacts={contacts} />
+    <ContactForm />
     </>
-  )
+  );
 }
 
 export default App
